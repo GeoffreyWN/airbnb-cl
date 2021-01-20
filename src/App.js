894efdff1bfd495819b70f0c9c29f3ab2858fb4a
geof <>
2021-01-20 16:38:49 +0300

    added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="App">
-      
+     <Header/>
+     <Home/>
+        {/* header */}
+        {/* banner */}
+          {/* search */}
+        {/* cards */}
+        {/* footer */}
+      {/* searchPage */}
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import LanguageIcon from "@material-ui/icons/Language";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { Avatar } from "@material-ui/core";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img
+        className="header__icon"
+        src="https://www.logo.wine/a/logo/Airbnb/Airbnb-Logo.wine.svg"
+        alt="logo"
+      />
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
+      <div className="header__right">
+        <p>Become a host</p>
+        <LanguageIcon />
+        <ExpandMoreIcon />
+        <Avatar />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
